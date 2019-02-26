a655549622168b5a4fa284250d730e7635f52bb3
Andy Meneely <andy@se.rit.edu>
2019-02-26 13:10:54 -0500

    Fix up curate_ready

--- scripts/curate_ready.rb
+++ scripts/curate_ready.rb
@@ -2,10 +2,10 @@ require_relative 'helpers'
 
 class CurateReady
   def print_readiness(print_ready=true)
-    cve_yamls.each do |yml_file|
+    cve_ymls.each do |yml_file|
       yml = File.open(yml_file) { |f| YAML.load(f) }
       any_fix = yml['fixes'].inject(false) do |memo, fix|
-        memo || !fix['commit'].empty? || !fix[:commit].empty?
+        memo || !fix['commit'].to_s.empty? || !fix[:commit].to_s.empty?
       end
       if !yml['curated']
         if any_fix
